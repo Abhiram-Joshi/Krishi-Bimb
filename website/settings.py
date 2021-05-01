92d6b000e370ba86b3bba8613eaa318ac40c9da7
Abhiram <abramjoshi@gmail.com>
2021-05-01 10:55:06 +0530

    fix: settings.py

--- website/settings.py
+++ website/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,5 +134,5 @@ LOGIN_URL = reverse_lazy("account:user_login")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+# # Activate Django-Heroku.
+# django_heroku.settings(locals())
